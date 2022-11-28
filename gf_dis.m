3a498d106c04b9ee821a8841bbaf9ddf2d71bcb4
zhangmingce <118955187+zhangmingce@users.noreply.github.com>
2022-11-28 16:19:55 +0800

    Delete gf_dis.m

--- gf_dis.m
+++
@@ -1,9 +0,0 @@
-function ret = gf_dis(a,b)
-ret = 0;
-44
-a_temp = de2bi(gf2num(a),3);
-b_temp = de2bi(gf2num(b),3);
-temp = a_temp+b_temp;
-temp = mod(temp,2);
-ret = sum(temp);
-%计算GF域内每个码元与“0”码字的码距，并映射在成实数子程序：
